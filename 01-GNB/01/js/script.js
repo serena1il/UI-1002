5ed0da6f40a99960d7538711f3fd86181e099a3e
serena1il <kangchaebin0629@naver.com>
2024-10-02 14:37:01 +0900

    최종완성

--- 01-GNB/01/js/script.js
+++ 01-GNB/01/js/script.js
@@ -2,13 +2,17 @@ $(function () {
     // 대상을 변수에 저장
     const $menu = $(".gnb > li");
     const $submenu = $(".submenu");
+    const duration = 300; //0.3초
 
     // 메뉴에 마우스가 들어왔을 떄
+    // .stop:현재 진행중인 애니메이션 즉시 중지
     $menu.on("mouseenter", function () {
-        $submenu.slideDown();
+        $(this).find($submenu).stop().slideDown(duration);
+        $(this).addClass("on");
     });
     // 메뉴에 마우스가 나갔왔을 떄
     $menu.on("mouseleave", function () {
-        $submenu.slideUp();
+        $submenu.stop().slideUp(duration);
+        $(this).removeClass("on");
     });
 });
